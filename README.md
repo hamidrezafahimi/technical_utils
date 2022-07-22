261d87b241622fbf446116d6bf4d380facb71255
hamidrezafahimi <hamidrfahimi@gmail.com>
2022-07-22 08:16:01 -0400

    got an imu and cmd csv file from tello bag

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Technical Utils
 
 A set of go-to programs archived together.
+
+`process_bagfiles` is a bunch of python scripts which handle the extraction and plotting of data saved in ROS bagfiles.
+
+`plotting` includes some useful programs for drawing plots in python's *matplotlib* platform.
--- process_bagfiles/bag_to_csv.py
+++ process_bagfiles/bag_to_csv.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python
 
 import rosbag
-from cv_bridge import CvBridge
 from nav_msgs.msg import Odometry
 import pandas as pd
 import csv
 import argparse
-from cv_bridge import CvBridge, CvBridgeError
 import cv2 as cv
 from pathlib import Path
 import numpy as np
@@ -17,10 +15,9 @@ parser.add_argument("file")
 args = parser.parse_args()
 print(args.file)
 
-bridge = CvBridge()
 
 startIdx = 0
-endIdx = 800
+endIdx = 80000
 
 # initPoint = np.array([29.578, 52.748, 4])
 initPoint = np.array([0, 0, 0])
@@ -37,30 +34,44 @@ def getFileName(path):
             break
     return path[k+1:end]
 
-xs = []
-ys = []
-zs = []
-ts = []
-cnt = 0
-startTime = 0
-# odom_cnt = 0
 
 bagFile = args.file
 bag = rosbag.Bag(bagFile)
 bagName = getFileName(bagFile)
-imgDir = bagName+"images/"
-img_cnt = 0
-Path(imgDir).mkdir(parents=True, exist_ok=True)
-for topic, msg, t in bag.read_messages(topics=['/gazebo/model_states']):
 
-   if topic == '/gazebo/model_states':
-       # print("odom: ", odom_cnt)
-       print('---------------------------------------------')
-       print(msg.pose[9])
-       # print(msg.pose[2].position.x)
-       xs.append(msg.pose[9].position.x)
-       ys.append(msg.pose[9].position.y)
-       zs.append(msg.pose[9].position.z)
+# Uncomment this part in a case that you need the images:
+# from cv_bridge import CvBridge, CvBridgeError
+# bridge = CvBridge()
+# imgDir = bagName+"images/"
+# img_cnt = 0
+# Path(imgDir).mkdir(parents=True, exist_ok=True)
+
+
+xs = []
+ys = []
+zs = []
+ws = []
+ts = []
+
+xs_a = []
+ys_a = []
+zs_a = []
+
+cnt = 0
+startTime = 0
+# odom_cnt = 0
+
+
+# for topic, msg, t in bag.read_messages(topics=['/gazebo/model_states']):
+
+#    if topic == '/gazebo/model_states':
+#        # print("odom: ", odom_cnt)
+#        print('---------------------------------------------')
+#        print(msg.pose[9])
+#        # print(msg.pose[2].position.x)
+#        xs.append(msg.pose[9].position.x)
+#        ys.append(msg.pose[9].position.y)
+#        zs.append(msg.pose[9].position.z)
        # odom_cnt += 1
 # for topic, msg, t in bag.read_messages(topics=['/tello/odom', '/tello/camera/image_raw']):
 #     cnt += 1
@@ -80,7 +91,25 @@ for topic, msg, t in bag.read_messages(topics=['/gazebo/model_states']):
    #     cv.imwrite(filename, image)
    #     img_cnt += 1
 
+for topic, msg, t in bag.read_messages(topics=['/tello/imu', '/tello/cmd_vel']):
+    cnt += 1
+    # if topic == '/tello/imu' and cnt >= startIdx and cnt <= endIdx:
+    #    xs.append(np.float32(msg.orientation.x))
+    #    ys.append(np.float32(msg.orientation.y))
+    #    zs.append(np.float32(msg.orientation.z))
+    #    ws.append(np.float32(msg.orientation.w))
+    #    if startTime == 0:
+    #        startTime = msg.header.stamp.to_sec()
+    #    print(msg.header.stamp.to_sec()-startTime)
+    #    ts.append(msg.header.stamp.to_sec()-startTime)
 
+    if topic == '/tello/cmd_vel' and cnt >= startIdx and cnt <= endIdx:
+        xs.append(np.float32(msg.linear.x))
+        ys.append(np.float32(msg.linear.y))
+        zs.append(np.float32(msg.linear.z))
+        xs_a.append(np.float32(msg.angular.x))
+        ys_a.append(np.float32(msg.angular.y))
+        zs_a.append(np.float32(msg.angular.z))
 
 
 # theta = 90.0*(np.pi/180)
@@ -88,9 +117,9 @@ for topic, msg, t in bag.read_messages(topics=['/gazebo/model_states']):
 # ys = np.array(ys)*np.cos(theta) - np.array(xs)*np.sin(theta)
 
 zeroDist = initPoint - np.array([xs[0], ys[0], zs[0]])
-print(initPoint)
-print(np.array([xs[0], ys[0], zs[0]]))
-print(zeroDist)
+# print(initPoint)
+# print(np.array([xs[0], ys[0], zs[0]]))
+# print(zeroDist)
 
 
 # print(ys[200])
@@ -103,18 +132,29 @@ print(zeroDist)
 Xs = np.array(ys)
 Ys = np.array(xs)
 Zs = np.array(zs)
+Xs_a = np.array(ys_a)
+Ys_a = np.array(xs_a)
+Zs_a = np.array(zs_a)
+# Ws = np.array(ws)
 
-xName = 'bagfile_'+bagName+'_x'
-yName = 'bagfile_'+bagName+'_y'
-zName = 'bagfile_'+bagName+'_z'
-
+# xName = 'bagfile_'+bagName+'_x'
+# yName = 'bagfile_'+bagName+'_y'
+# zName = 'bagfile_'+bagName+'_z'
+# wName = 'bagfile_'+bagName+'_w'
+xName = 'x_lin'
+yName = 'y_lin'
+zName = 'z_lin'
+# wName = 'w'
+xName_a = 'x_ang'
+yName_a = 'y_ang'
+zName_a = 'z_ang'
 
 # dict = {xName: xs[:-3100], yName: ys[:-3100], zName: zs[:-3100]}
-dict = {xName: Xs, yName: Ys, zName: Zs}
-#
+# dict = {xName: Xs, yName: Ys, zName: Zs, wName: Ws, 'time': ts}
+dict = {xName: Xs, yName: Ys, zName: Zs, xName_a: Xs_a, yName_a:Ys_a, zName_a:Zs_a}
+
 df = pd.DataFrame(dict)
 
-# # saving the dataframe
 fileName = 'position_'+bagName+'.csv'
 
 df.to_csv(fileName)
