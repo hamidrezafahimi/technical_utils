c73a95141aaa25b774ddeed0d81f07a84640ba8b
hamidrezafahimi <hamidrfahimi@gmail.com>
2022-08-11 08:21:21 -0400

    working on webots_ros2_camera: still not removed initial scripts

--- ROS2/webots_ws/src/webots_ros2_camera/webots_ros2_camera/camera_driver.py
+++ ROS2/webots_ws/src/webots_ros2_camera/webots_ros2_camera/camera_driver.py
@@ -1,21 +1,21 @@
 import rclpy
 from geometry_msgs.msg import Twist
 
-HALF_DISTANCE_BETWEEN_WHEELS = 0.045
-WHEEL_RADIUS = 0.025
+# HALF_DISTANCE_BETWEEN_WHEELS = 0.045
+# WHEEL_RADIUS = 0.025
 
 class MyRobotDriver:
     def init(self, webots_node, properties):
         self.__robot = webots_node.robot
 
-        self.__left_motor = self.__robot.getDevice('left wheel motor')
-        self.__right_motor = self.__robot.getDevice('right wheel motor')
+        # self.__left_motor = self.__robot.getDevice('left wheel motor')
+        # self.__right_motor = self.__robot.getDevice('right wheel motor')
 
-        self.__left_motor.setPosition(float('inf'))
-        self.__left_motor.setVelocity(0)
+        # self.__left_motor.setPosition(float('inf'))
+        # self.__left_motor.setVelocity(0)
 
-        self.__right_motor.setPosition(float('inf'))
-        self.__right_motor.setVelocity(0)
+        # self.__right_motor.setPosition(float('inf'))
+        # self.__right_motor.setVelocity(0)
 
         # self.__camera = self.__robot.getCamera('cam_1')
         # print('I got the camera')
@@ -25,19 +25,19 @@ class MyRobotDriver:
 
         rclpy.init(args=None)
         self.__node = rclpy.create_node('camera_driver')
-        self.__node.create_subscription(Twist, 'cmd_vel', self.__cmd_vel_callback, 1)
+        # self.__node.create_subscription(Twist, 'cmd_vel', self.__cmd_vel_callback, 1)
 
-    def __cmd_vel_callback(self, twist):
-        self.__target_twist = twist
+    # def __cmd_vel_callback(self, twist):
+        # self.__target_twist = twist
 
     def step(self):
         rclpy.spin_once(self.__node, timeout_sec=0)
 
-        forward_speed = self.__target_twist.linear.x
-        angular_speed = self.__target_twist.angular.z
+        # forward_speed = self.__target_twist.linear.x
+        # angular_speed = self.__target_twist.angular.z
 
-        command_motor_left = (forward_speed - angular_speed * HALF_DISTANCE_BETWEEN_WHEELS) / WHEEL_RADIUS
-        command_motor_right = (forward_speed + angular_speed * HALF_DISTANCE_BETWEEN_WHEELS) / WHEEL_RADIUS
+        # command_motor_left = (forward_speed - angular_speed * HALF_DISTANCE_BETWEEN_WHEELS) / WHEEL_RADIUS
+        # command_motor_right = (forward_speed + angular_speed * HALF_DISTANCE_BETWEEN_WHEELS) / WHEEL_RADIUS
 
-        self.__left_motor.setVelocity(command_motor_left)
-        self.__right_motor.setVelocity(command_motor_right)
+        # self.__left_motor.setVelocity(command_motor_left)
+        # self.__right_motor.setVelocity(command_motor_right)
